ed461cda51d560c287926078b6766866e8929308
Santiago González Erazo <santiago.gonzalez.erazo@gmail.com>
2018-01-24 20:00:46 +0100

    update

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -29,10 +29,27 @@ describe 'Kata' do
     expect(page).to have_content(kata_description)
   end
 
+  it 'can be updated' do
+    kata_title = 'New Kata title'
+    kata_description = 'New Kata description'
+    create_kata
+
+    visit root_path
+    click_on('Edit')
+    fill_in(:kata_title, with: kata_title)
+    fill_in(:kata_description, with: kata_description)
+    click_on('Save')
+
+    expect(page).to have_content(kata_title)
+    expect(page).to have_content(kata_description)
+  end
+
   def create_kata(title: 'Kata title', description: 'Kata description')
     kata = Kata.new(title: title, description: description)
     kata.save
 
     kata
   end
+
+
 end
