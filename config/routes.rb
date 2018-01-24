67e375c549d9bd767ee3e602dc078cf3a2cf10e9
Santiago González Erazo <santiago.gonzalez.erazo@gmail.com>
2018-01-24 20:30:18 +0100

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   post '/create/katas' => 'katas#create', as: :create_katas
   get '/edit/katas/:id' => 'katas#edit', as: :edit_kata
   patch '/katas/:id' => 'katas#update', as: :update_kata
+  delete 'katas/:id' => 'katas#delete', as: :delete_kata
 end
